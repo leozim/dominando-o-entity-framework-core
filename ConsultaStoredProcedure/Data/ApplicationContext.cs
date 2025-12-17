c6ede77f017319cd4327036a6a943fa8883fc370
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2025-12-17 17:18:06 -0300

    setup do novo projeto do curso

---
+++ ConsultaStoredProcedure/Data/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using DominandoEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DominandoEFCore.Data;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<Departamento> Departamentos { get; set; }
+    public DbSet<Funcionario> Funcionarios { get; set; }
+
+    override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        const string strConnection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ConsultaStoredProcedure;Integrated Security=True;pooling=True";
+
+        optionsBuilder
+            .UseSqlServer(strConnection)
+            .EnableSensitiveDataLogging()
+            // .UseLazyLoadingProxies()
+            .LogTo(Console.WriteLine, LogLevel.Information);
+    }
+}
